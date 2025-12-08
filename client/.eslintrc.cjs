210d95cde057128031bfb12dfb8ae707020dffdc
YutoMatui <157707374+YutoMatui@users.noreply.github.com>
2025-12-08 19:48:21 +0900

    Fix lint config to pass Vercel build

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -8,23 +8,13 @@ module.exports = {
     ],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
     parser: '@typescript-eslint/parser',
-    module.exports = {
-        root: true,
-        env: { browser: true, es2020: true },
-        extends: [
-            'eslint:recommended',
-            'plugin:@typescript-eslint/recommended',
-            'plugin:react-hooks/recommended',
+    plugins: ['react-refresh'],
+    rules: {
+        'react-refresh/only-export-components': [
+            'warn',
+            { allowConstantExport: true },
         ],
-        ignorePatterns: ['dist', '.eslintrc.cjs'],
-        parser: '@typescript-eslint/parser',
-        plugins: ['react-refresh'],
-        rules: {
-            'react-refresh/only-export-components': [
-                'warn',
-                { allowConstantExport: true },
-            ],
-            '@typescript-eslint/no-explicit-any': 'off',
-            '@typescript-eslint/no-unused-vars': 'off',
-        },
-    }
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+    },
+}
